682fad4849ed10c610ce100eb54e19411ae7520b
max <max.teh.lueser@gmail.com>
2016-05-10 16:04:38 +0100

    Refactoring

--- app/src/main/java/com/sonicmax/tinymet/MainActivity.java
+++ app/src/main/java/com/sonicmax/tinymet/MainActivity.java
@@ -3,7 +3,6 @@ package com.sonicmax.tinymet;
 import android.content.res.Configuration;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,11 +12,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.sonicmax.tinymet.adapters.DictionaryAdapter;
 import com.sonicmax.tinymet.dialogs.LanguageDialog;
+import com.sonicmax.tinymet.dialogs.SortDialog;
 import com.sonicmax.tinymet.dialogs.TempoRangeDialog;
 import com.sonicmax.tinymet.utilities.Tempo;
 import com.sonicmax.tinymet.utilities.TempoDictionary;
@@ -25,19 +26,32 @@ import com.sonicmax.tinymet.utilities.TempoDictionary;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Default values
     private final String DEFAULT_LANG = "Italian";
+
+    // Fragment tags
     private final String LANGUAGE_SELECTOR_TAG = "language_selector";
     private final String TEMPO_RANGE_TAG = "tempo_selector";
+    private final String SORT_TAG = "sort_selector";
 
+    // sqlite stuff
+    private final String NAME_COLUMN = "NAME";
+    private final String MIN_COLUMN = "MIN";
+    private final String MAX_COLUMN = "MAX";
+
+    // Objects, primitives and views that we might want to reference later
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private ListView mDrawerDictionary;
     private TextView mDrawerLanguage;
+    private ImageButton mDrawerSort;
     private DictionaryAdapter mDrawerAdapter;
     private TempoDictionary mTempoDictionary;
+    private String mCurrentLanguage;
 
     ///////////////////////////////////////////////////////////////////////////
-    // Lifecycle stuff
+    // Activity lifecycle stuff
     ///////////////////////////////////////////////////////////////////////////
 
     @Override
@@ -55,9 +69,7 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
         }
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLanguage = (TextView) findViewById(R.id.drawer_lang);
-        mDrawerDictionary = (ListView) findViewById(R.id.drawer_dictionary);
+        getUiElements();
 
         mTempoDictionary = new TempoDictionary(this) {
             @Override
@@ -75,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onChooseLanguage(String language) {
                         mDrawerDictionary.clearChoices();
+                        mCurrentLanguage = language;
                         mDrawerLanguage.setText(language);
                         mTempoDictionary.loadDatabase(language);
                         this.dismiss();
@@ -85,8 +98,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mDrawerSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final SortDialog selector = new SortDialog() {
+
+                    @Override
+                    public void onChooseSort(String sortType) {
+                        mDrawerDictionary.clearChoices();
+                        mTempoDictionary.sortDatabase(mCurrentLanguage, sortType);
+                        this.dismiss();
+                    }
+                };
+
+                selector.show(getSupportFragmentManager(), SORT_TAG);
+            }
+        });
+
         mDrawerLanguage.setText(DEFAULT_LANG);
         mTempoDictionary.loadDatabase(DEFAULT_LANG);
+        mCurrentLanguage = DEFAULT_LANG;
+    }
+
+    private void getUiElements() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLanguage = (TextView) findViewById(R.id.drawer_lang);
+        mDrawerDictionary = (ListView) findViewById(R.id.drawer_dictionary);
+        mDrawerSort = (ImageButton) findViewById(R.id.sort_dictionary);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -97,9 +135,9 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> nameArray = new ArrayList<>();
         final ArrayList<Tempo> tempoArray = new ArrayList<>();
 
-        int nameColumn = cursor.getColumnIndex("NAME");
-        int minColumn = cursor.getColumnIndex("MIN");
-        int maxColumn = cursor.getColumnIndex("MAX");
+        int nameColumn = cursor.getColumnIndex(NAME_COLUMN);
+        int minColumn = cursor.getColumnIndex(MIN_COLUMN);
+        int maxColumn = cursor.getColumnIndex(MAX_COLUMN);
 
         while (cursor.moveToNext()) {
             nameArray.add(cursor.getString(nameColumn));
